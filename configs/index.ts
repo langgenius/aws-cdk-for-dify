4c35df4059b4b042da7ae1ab969e4eb5e4f92bfb
Petrus Han <petrus.hanks@gmail.com>
2024-10-21 12:33:55 +0800

    update README and add a prefix to identify the different environments in the same region. (#11)
    * add a random value into resource name prefix
    
    In some scenarios, such as when cdk creates multiple environments in the same region, naming conflicts can occur.
    
    * Update README.md
    
    add more details and claims the production setup process
    
    * Update README.md
    
    fix link to private repo
    
    ---------
    
    Co-authored-by: Xiyuan Chen <52963600+GareArc@users.noreply.github.com>

--- configs/index.ts
+++ configs/index.ts
@@ -1,4 +1,4 @@
-import { PRODUCT_NAME } from "./constants";
+import { PRODUCT_NAME, AWS_RESOURCE_SUFFIX } from "./constants";
 import { prodConfig, ProdStackConfig } from "./prod";
 import { StackConfig } from "./stackConfig";
 import { testConfig, TestStackConfig } from "./test";
@@ -14,5 +14,8 @@ export const config: DifyCDKConfig = {
 }
 
 export const getConstructPrefix = (config: StackConfig) => {
-  return `${PRODUCT_NAME}-${config.environment}`;
+  // Generate a 6-character random string consisting of numbers and letters
+  const randomSuffix = Math.random().toString(36).slice(2, 8).toUpperCase();
+
+  return `${PRODUCT_NAME}-${config.environment}-${AWS_RESOURCE_SUFFIX}`;
 } 
